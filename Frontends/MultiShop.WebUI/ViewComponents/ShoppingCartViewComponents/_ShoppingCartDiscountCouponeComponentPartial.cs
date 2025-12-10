9103d6ecbc58e380098b6de81114e7abdf5b4761
Çağrı <cagrikandemir58@icloud.com>
2025-12-10 15:43:01 +0300

    Default Tasarımı Genel Olarak Tamamlandı Admin Panel Tasarımına Geçtim

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ShoppingCartViewComponents/_ShoppingCartDiscountCouponeComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewComponents
+{
+    public class _ShoppingCartDiscountCouponeComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/UIAdminLayoutComponents/_ScriptUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UIAdminLayoutComponents
+{
+    public class _ScriptUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
