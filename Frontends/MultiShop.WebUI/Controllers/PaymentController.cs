8941ebdc3f0d912f0d0014fcd59574c8fc0a51af
Çağrı <cagrikandemir58@icloud.com>
2026-08-11 11:51:21 +0300

    Kredi Kart Script Tamamlandı

---
+++ Frontends/MultiShop.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "MultiShop";
+            ViewBag.directory2 = "Ödeme";
+            ViewBag.directory3 = "Kartlı Ödeme";
+            return View();
+        }
+    }
+}
