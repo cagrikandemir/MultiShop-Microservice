56059c4843dfd1a59d1c83b6bdcee0b5c99eac48
Çağrı <cagrikandemir58@icloud.com>
2026-08-18 18:51:44 +0300

    fix

--- Services/Images/MultiShop.ImageWebUI/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace MultiShop.ImageWebUI.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
