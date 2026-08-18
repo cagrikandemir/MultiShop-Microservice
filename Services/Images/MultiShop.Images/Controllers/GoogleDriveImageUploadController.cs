32df38c001a2ed5f3b3ded0e07d1698b13152a5d
Çağrı <cagrikandemir58@icloud.com>
2026-08-18 18:54:52 +0300

    Fix Portnumber

--- Services/Images/MultiShop.Images/Controllers/GoogleDriveImageUploadController.cs
+++ Services/Images/MultiShop.Images/Controllers/GoogleDriveImageUploadController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MultiShop.Images.Controllers
 {
+    [AllowAnonymous]
     [Route("[controller]")]
     [ApiController]
     public class GoogleDriveImageUploadController : ControllerBase
