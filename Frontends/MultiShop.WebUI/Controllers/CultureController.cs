310f1e9eb3a8250a03ac54613ae5ca0dd26d6275
Çağrı <cagrikandemir58@icloud.com>
2026-08-20 13:40:46 +0300

    Localization Fixed Ana Sayfa İçin tekrar bakılıcak Proje Tamamlandı

---
+++ Frontends/MultiShop.WebUI/Controllers/CultureController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class CultureController : Controller
+    {
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            // Tarayıcıya dil bilgisi içeren bir çerez bırakılır
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)), new CookieOptions
+            {
+                Expires = DateTimeOffset.UtcNow.AddYears(1) // 1 yıl geçerli olur
+            });
+
+            // Kullanıcıyı geldiği sayfaya geri yönlendirilır
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
